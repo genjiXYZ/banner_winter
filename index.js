1924105edd9bde641478fb27938edc6eb1ca7f7a
genjiXYZ <895939059@qq.com>
2021-01-18 15:21:41 +0800

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var domBanner = document.querySelector(".banner");
 
 var myCanvas = document.querySelector(".snow");
 
-var locked = false
+
 var reqSign
 var per = 0;
 var enterX = 0
@@ -12,63 +12,54 @@ domBanner.addEventListener("mouseenter", (e) => {
 	enterX = e.clientX
 	domBanner.classList.add("moving");
 })
-
-domBanner.addEventListener("mousemove", (e) => {
-	Move(e, enterX, domBanner);
-});
+domBanner.onmousemove = (e) => Move(e, enterX, domBanner);
 
 domBanner.addEventListener("mouseleave", (e) => {
 	cancleNextAnime()
 	domBanner.style.setProperty("--per", 0);
 	domBanner.classList.remove("moving");
-	let step =   (Math.abs(per) / (300 / 16)).toFixed(3) * 1
-	console.log('step: ', step);
-	SnowAnime.leave(step)
+	//transition time   ratio /  ( 0.3s / 1fps )
+	SnowAnime.leave((Math.abs(per) / (300 / 16)).toFixed(3) * 1)
 })
 
-window.addEventListener('resize',()=>{
+window.addEventListener('resize', () => {
 	onResize()
 })
 
 
 function onResize() {
 	let myLayer = document.querySelectorAll('.layer')
-	console.log('myLayer: ', myLayer);
 	myLayer.forEach(e => {
-		e.children[0].style.width = `${window.innerWidth * 1.2}px`
-	
+		if (e.children[0].nodeName === 'CANVAS') {
+			e.children[0].style.width = `${document.querySelector('.banner').clientWidth * 1}px`
+		}
+		e.children[0].style.width = `${document.querySelector('.banner').clientWidth * 1.2}px`
 	})
 };
 
 function Move(e, enterX, dom) {
-	if (!locked) {
-		locked = true;
+	if (!reqSign) {
 		reqSign = requestAnimationFrame(() => {
+			reqSign = null
 			per = (e.clientX - enterX) / dom.clientWidth;
-
 			dom.style.setProperty("--per", per);
-			locked = false;
 		});
 	}
 };
 function cancleNextAnime() {
 	cancelAnimationFrame(reqSign)
-	locked = false
+	reqSign = null
 }
 
-	window.onblur = () => {
-		SnowAnime.stop()
-		document.title = "雪停了"
-	};
-	window.onfocus = () => {
-		SnowAnime.stop();
-		SnowAnime.start();
-		document.title = "下雪中"
-	};
-
-
-
-
-
-	var SnowAnime = new Snow(myCanvas);
+window.onblur = () => {
+	SnowAnime.stop()
+	document.title = "雪停了"
+};
+window.onfocus = () => {
+	SnowAnime.stop();
 	SnowAnime.start();
+	document.title = "下雪中"
+};
+
+var SnowAnime = new Snow(myCanvas);
+SnowAnime.start();
--- snow.js
+++ snow.js
@@ -1,112 +1,104 @@
 class Snowflake {
-	constructor() {
-		this.x = 0;
-		this.y = 0;
-		this.vx = 0;
-		this.vy = 0;
-		this.radius = 0;
-		this.alpha = 0;
-		this.reset();
-	}
-	reset() {
-		this.x = Math.floor(this.randBetween(0, window.innerWidth));
-		this.y = -155;
-		this.vx = 0;
-		// this.vy = this.randBetween(1, 2);
-		this.radius = this.randBetween(2, 4).toFixed(4) * 1;
-		if (this.radius >= 3.5) {
-			this.vy = this.randBetween(0.8, 1.5).toFixed(4) * 1
-			this.alpha = this.randBetween(0.5, 1);
-		} else {
-			this.vy = this.randBetween(0.5, 1).toFixed(4) * 1;
-			this.alpha = this.randBetween(0.1, 1);
-		}
-	}
-	randBetween(min, max) {
-		return min + Math.random() * (max - min);
-	}
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.radius = 0;
+    this.alpha = 0;
+    this.reset();
+  }
+  reset() {
+    this.x = Math.floor(this.randBetween(0, window.innerWidth));
+    this.y = -155;
+    this.vx = 0;
+    // this.vy = this.randBetween(1, 2);
+    this.radius = this.randBetween(2, 4).toFixed(4) * 1;
+    if (this.radius >= 3.5) {
+      this.vy = this.randBetween(0.8, 1.5).toFixed(4) * 1;
+      this.alpha = this.randBetween(0.5, 1);
+    } else {
+      this.vy = this.randBetween(0.5, 1).toFixed(4) * 1;
+      this.alpha = this.randBetween(0.1, 1);
+    }
+  }
+  randBetween(min, max) {
+    return min + Math.random() * (max - min);
+  }
 
-	update() {
-		this.x += this.vx;
-		this.y += this.vy;
+  update(height) {
+    this.x += this.vx;
+    this.y += this.vy;
 
-		if (this.y + this.radius > 155) {
-			this.reset();
-		}
-	}
+    if (this.y + this.radius > height) {
+      this.reset();
+    }
+  }
 }
 
 class Snow {
-	constructor(el) {
-		this.canvas = el;
-		this.ctx = this.canvas.getContext("2d");
-		window.addEventListener("resize", () => this.onResize());
-		this.onResize();
-		this.updateBound = this.update.bind(this);
-		this.createSnowflakes();
-	}
+  constructor(el) {
+    this.canvas = el;
+    this.ctx = this.canvas.getContext("2d");
+    this.updateBound = this.update.bind(this);
+    this.flakesNumber = Math.floor(window.innerWidth / 8);
+    window.addEventListener("resize", () => this.onResize());
+    this.onResize();
+    this.createSnowflakes();
+  }
 
-	onResize() {
-		this.width = window.innerWidth;
-		this.height = window.innerHeight;
-		this.canvas.width = this.width;
-		this.canvas.height = 155;
-	}
+  onResize() {
+    this.width =document.querySelector('.banner').clientWidth ;
+    this.height = document.querySelector('.banner').clientHeight;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+  }
 
-	createSnowflakes() {
-		const flakes = window.innerWidth / 10;
+  createSnowflakes() {
+    const flakes = this.flakesNumber;
+    this.snowflakes = [];
+    for (let s = 0; s < flakes; s++) {
+      this.snowflakes.push(new Snowflake());
+    }
+  }
 
-		this.snowflakes = [];
-		for (let s = 0; s < flakes; s++) {
-			this.snowflakes.push(new Snowflake());
-		}
-	}
+  update() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.snowflakes.forEach((flake) => {
+      flake.update(this.height);
+      this.ctx.save();
+      this.ctx.fillStyle = "#FFF";
+      this.ctx.beginPath();
 
-	update() {
-		this.ctx.clearRect(0, 0, this.width, this.height);
+      if (flake.radius >= 3) {
+        this.ctx.arc(flake.x + per * 200, flake.y, flake.radius, 0, Math.PI * 2)
+      } else {
+        this.ctx.arc(flake.x + per * 80, flake.y, flake.radius, 0, Math.PI * 2);
+      }
+      this.ctx.closePath();
+      this.ctx.globalAlpha = flake.alpha;
+      this.ctx.fill();
+      this.ctx.restore();
+    });
 
-		for (let flake of this.snowflakes) {
-			flake.update();
-
-			this.ctx.save();
-			this.ctx.fillStyle = "#FFF";
-			this.ctx.beginPath();
-			if (flake.radius >= 3) {
-				this.ctx.arc(
-					flake.x + per * 160,
-					flake.y,
-					flake.radius,
-					0,
-					Math.PI * 2
-				);
-			} else {
-				this.ctx.arc(flake.x + per * 80, flake.y, flake.radius, 0, Math.PI * 2);
-			}
-			this.ctx.closePath();
-			this.ctx.globalAlpha = flake.alpha;
-			this.ctx.fill();
-			this.ctx.restore();
-		}
-		this.reqFlag = requestAnimationFrame(this.updateBound);
-	}
-	start() {
-		this.reqFlag = requestAnimationFrame(this.updateBound);
-	}
-	stop() {
-		cancelAnimationFrame(this.reqFlag);
-	}
-	leave = (step) => {
-		console.log("step: ", step);
-
-		if (per > 0 && per - step > 0) {
-			per -= step;
-			this.perReqFlag = requestAnimationFrame(() => this.leave(step));
-		} else if (per < 0 && per + step < 0) {
-			per += step;
-			this.perReqFlag = requestAnimationFrame(() => this.leave(step));
-		} else {
-			per = 0;
-			cancelAnimationFrame(this.perReqFlag);
-		}
-	};
+    this.reqFlag = requestAnimationFrame(this.updateBound);
+  }
+  start() {
+    this.reqFlag = requestAnimationFrame(this.updateBound);
+  }
+  stop() {
+    cancelAnimationFrame(this.reqFlag);
+  }
+  leave = (step) => {
+    if (per > 0 && per - step > 0) {
+      per -= step;
+      this.perReqFlag = requestAnimationFrame(() => this.leave(step));
+    } else if (per < 0 && per + step < 0) {
+      per += step;
+      this.perReqFlag = requestAnimationFrame(() => this.leave(step));
+    } else {
+      per = 0;
+      cancelAnimationFrame(this.perReqFlag);
+    }
+  };
 }
